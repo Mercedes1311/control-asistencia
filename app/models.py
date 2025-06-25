d91789250fefbc3cc6c3eb7e19eeae604d12a165
Mercedes1311 <nayelydurand2002@gmail.com>
2025-06-25 12:12:12 -0500

    version 2

--- app/models.py
+++ app/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-from django.db import models
-
 class Empleado(models.Model):
     id_empleado = models.AutoField(primary_key=True)
     nombres = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
-    dni = models.IntegerField()
+    dni = models.IntegerField(unique=True)
     contrato = models.CharField(max_length=50)
 
     def __str__(self):
@@ -14,7 +12,7 @@ class Empleado(models.Model):
 
 class TipoAsistencia(models.Model):
     id_tipo = models.AutoField(primary_key=True)
-    nombre_asistencia = models.CharField(max_length=50)
+    nombre_asistencia = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.nombre_asistencia
@@ -25,10 +23,8 @@ class RegistroAsistencia(models.Model):
     tipo = models.ForeignKey(TipoAsistencia, on_delete=models.CASCADE)
     fecha_registro = models.DateField()
     hora_registro = models.TimeField()
-    descripcion = models.CharField(max_length=50, blank=True, null=True)
-
+    descripcion = models.CharField(max_length=50, blank=True, null=True)   
+    fingerprint = models.CharField(max_length=100, blank=True, null=True) # FingerprintJS ID del dispositivo
 
     def __str__(self):
-        return f"{self.empleado} - {self.fecha_registro} {self.hora_registro}"
-
-
+        return f"{self.empleado} - {self.tipo.nombre_asistencia} - {self.fecha_registro} {self.hora_registro}"
