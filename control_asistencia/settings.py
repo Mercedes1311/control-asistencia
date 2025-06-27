3f19e4c40371b816068ff0e7c3e50779001ddfd7
Mercedes1311 <nayelydurand2002@gmail.com>
2025-06-27 10:22:36 -0500

    configuracion settings

--- control_asistencia/settings.py
+++ control_asistencia/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -71,27 +76,33 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'control_asistencia.wsgi.application'
 
+DB_LIVE=os.getenv("DB_LIVE")
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gestion_asistencia',       # Nombre de la base de datos
-        'USER': 'nakamasac',                # Nuevo nombre de usuario
-        'PASSWORD': 'N4K4M425',             # Nueva contraseña
-        'HOST': 'localhost',                # Dirección del servidor
-        'PORT': '5432',                     # Puerto por defecto de PostgreSQL
+if DB_LIVE in [False,"False"]:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("DB_NAME"),       # Nombre de la base de datos
+            'USER': os.getenv("DB_USER"),               # Nuevo nombre de usuario
+            'PASSWORD': os.getenv("DB_PASSWORD"),             # Nueva contraseña
+            'HOST': os.getenv("DB_HOST"),              # Dirección del servidor
+            'PORT': os.getenv("DB_PORT"),                     # Puerto por defecto de PostgreSQL
+        }
+    }
 
 
 
@@ -130,6 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
